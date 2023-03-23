7036e9fe7e0217227c56d7738cd3caefb77aec2e
Aristo Bela <aristo.bela13@gmail.com>
2023-03-23 15:38:04 -0700

    week 2 day4

---
+++ module.py
@@ -0,0 +1,9 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+# module assignment
+
+
+side = int(input("This is it:"))
+Area = side*side
+print("Area of the square="+str(Area))
